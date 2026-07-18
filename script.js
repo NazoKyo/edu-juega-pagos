7fa454230edeb2c0a3ad2cd789a185336ce5ad53
Nazo <sonicmario23@hotmail.com>
2026-07-18 09:07:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+async function copiarTexto(idElemento, idBoton) {
 
+    const texto = document.getElementById(idElemento).innerText;
+
+    await navigator.clipboard.writeText(texto);
+
+    const boton = document.getElementById(idBoton);
+
+    const textoOriginal = boton.innerHTML;
+
+    boton.innerHTML = "✔ Copiado";
+
+    setTimeout(() => {
+
+        boton.innerHTML = textoOriginal;
+
+    }, 2000);
+
+}
+
+document.getElementById("btnCuenta").addEventListener("click", () => {
+    copiarTexto("cuenta", "btnCuenta");
+});
+
+document.getElementById("btnClabe").addEventListener("click", () => {
+    copiarTexto("clabe", "btnClabe");
+});
